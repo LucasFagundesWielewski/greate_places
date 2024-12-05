4eea29f9e8626fa38f5d681d7566136a651025b6
Lucas <lucas.fwielewski@gmail.com>
2024-12-04 23:37:39 -0300

    Refactor PlacesListScreen to use GreatPlaces provider for dynamic item listing

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:greate_places/providers/grate_places.dart';
 import 'package:greate_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -18,8 +20,26 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: const Center(
+          child: Text('Nenhum local cadastrado!'),
+        ),
+        builder: (ctx, greatPlaces, child) => greatPlaces.itemCount == 0
+            ? child!
+            : ListView.builder(
+                itemCount: greatPlaces.itemCount,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                      greatPlaces.getItemByIndex(i).image,
+                    ),
+                  ),
+                  title: Text(greatPlaces.getItemByIndex(i).title),
+                  onTap: () {
+                    // Go to detail page
+                  },
+                ),
+              ),
       ),
     );
   }
